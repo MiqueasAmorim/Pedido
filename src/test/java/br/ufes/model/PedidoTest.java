68914f17a49dce02d1c7e2999c04d058f59026da
Miqueas Amorim <miqueas.at@gmail.com>
2020-11-17 22:57:53 -0300

    Novos casos de testes para Pedido

--- src/test/java/br/ufes/model/PedidoTest.java
+++ src/test/java/br/ufes/model/PedidoTest.java
@@ -5,6 +5,8 @@
  */
 package br.ufes.model;
 
+import exception.ItemNaoEncontradoException;
+import exception.QuantidadeProdutoException;
 import java.time.LocalDate;
 import java.util.Optional;
 import org.junit.Assert;
@@ -103,5 +105,47 @@ public class PedidoTest {
         
         assertEquals(19.0, pedido.getValorAPagar(), 0.001);
         
+    }
+    
+    // Adicionar produto com uma quantidade igual a zero deve levantar exceção QuantidadeProdutoException
+    @Test
+    public void CT06() {
+        Pedido pedido = new Pedido(
+                new Cliente("Fulano", "123.456.789-01"),
+                new Produto("Caneta", 1.5, 20), 10,
+                LocalDate.now()
+        );
+        
+        Produto produto = new Produto("Lápis", 1.0, 10); 
+
+        Exception e = Assert.assertThrows(QuantidadeProdutoException.class, () -> pedido.addItem(produto, 0));
+        assertEquals("Informe uma quantidade válida!", e.getMessage());
+    }
+    
+    // Tentar remover um item que não está na lista de itens do pedido deve levantar exceção ItemNaoEncontradoException
+    @Test
+    public void CT07() {
+        Pedido pedido = new Pedido(
+                new Cliente("Fulano", "123.456.789-01"),
+                new Produto("Caneta", 1.5, 20), 10,
+                LocalDate.now()
+        );
+        
+        Exception e = Assert.assertThrows(ItemNaoEncontradoException.class, () -> pedido.removerItem("borracha azul"));
+        assertEquals("Item borracha azul não encontrado", e.getMessage());
+    }
+    
+    // Deve retornar o valor do desconto corretamente
+    // Pedido contendo 1 produto com valor de R$ 10,00
+    // O valor de desconto deve ser R$ 0,5
+    @Test
+    public void CT08() {
+        Pedido pedido = new Pedido(
+                new Cliente("Fulano", "123.456.789-01"),
+                new Produto("Caneta", 10, 20), 1,
+                LocalDate.now()
+        );
+        
+        assertEquals(0.5, pedido.getValorDesconto(), 0.01);
     }
 }
